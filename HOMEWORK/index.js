dd309f74a05d12c0aab38c1d1782281000559f25
AdnanH155 <102908608+AdnanH155@users.noreply.github.com>
2023-10-06 10:53:30 +0100

    Add files via upload

---
+++ HOMEWORK/index.js
@@ -0,0 +1,6 @@
+import { add } from "./utils.js";
+add(2, 2);
+console.log(add(2, 2));
+
+import random, { add } from "./utils.js";
+console.log(random(1, 1));
---
+++ HOMEWORK/utils.js
@@ -0,0 +1,12 @@
+const add = (x, y) => {
+  return x + y;
+};
+
+// 3
+export function add(x, y) {
+  return x + y;
+}
+// 4
+export default function (x, y) {
+  return x + y;
+}
